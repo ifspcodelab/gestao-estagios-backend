2a3d96b110c0db26bca5ee364e7a8e91facea531
Domingos Latorre <domingos.latorre@gmail.com>
2021-08-02 20:27:22 -0300

    feat: add user already exists exception handler

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/UserController.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/UserController.java
@@ -1,13 +1,13 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.user;
 
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.exceptions.ProblemDetail;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.exceptions.Violation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
@@ -22,5 +22,16 @@ public class UserController {
     public ResponseEntity<UserDto> create(@Valid @RequestBody UserCreateDto userCreateDto) {
         User user = this.userService.create(userCreateDto.getEmail(), userCreateDto.getPassword());
         return new ResponseEntity<>(new UserDto(user.getId(), user.getEmail()), HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ProblemDetail> alreadyExists(UserAlreadyExistsException exception) {
+        return new ResponseEntity<>(
+            new ProblemDetail(
+                "User already exists",
+                List.of(new Violation("email", "User already exists with email " + exception.getEmail()))
+            ),
+            HttpStatus.CONFLICT
+        );
     }
 }
