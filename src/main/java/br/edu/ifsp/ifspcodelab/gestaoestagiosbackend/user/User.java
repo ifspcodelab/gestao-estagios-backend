1bddd0f9cb72282a34c85010af3feaafa65eda76
Domingos Latorre <domingos.latorre@gmail.com>
2021-08-02 18:40:24 -0300

    feat: define user class as jpa entity

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/User.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/User.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
+@Entity
 @NoArgsConstructor
 @Getter
 @Setter
 public class User {
+    @Id
     private UUID id;
     private String email;
     private String password;
