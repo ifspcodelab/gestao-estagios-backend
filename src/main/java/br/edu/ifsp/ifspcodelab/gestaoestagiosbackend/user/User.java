4a033a3f5208db4d97afb0441e74d0d7f16d23fb
Domingos Latorre <domingos.latorre@gmail.com>
2021-08-02 17:01:26 -0300

    refactor: use lombok annotations in user class

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/User.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/User.java
@@ -1,42 +1,22 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.user;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     private UUID id;
     private String email;
     private String password;
 
-    public User() {
-    }
-
     public User(String email, String password) {
         this.id = UUID.randomUUID();
         this.email = email;
-        this.password = password;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
