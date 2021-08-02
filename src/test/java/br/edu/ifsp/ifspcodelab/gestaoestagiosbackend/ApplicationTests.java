84ea1fa29deb73f4048c433de0fa29bbad4743ff
Domingos Latorre <domingos.latorre@gmail.com>
2021-08-02 16:30:26 -0300

    chore: setup spring boot project

---
+++ src/test/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
