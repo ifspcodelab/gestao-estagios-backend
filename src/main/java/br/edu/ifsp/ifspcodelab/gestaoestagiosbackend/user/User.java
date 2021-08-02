8a775431dcf43ef2dd9ea1503bf16d9d11329c81
Domingos Latorre <domingos.latorre@gmail.com>
2021-08-02 18:54:40 -0300

    feat: define table name in entity user class

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/User.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/User.java
@@ -6,9 +6,11 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 @NoArgsConstructor
 @Getter
 @Setter
