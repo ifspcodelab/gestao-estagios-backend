a3df49aceeb9fffdf6c7a383e664cf456dc75144
Domingos Latorre <domingos.latorre@gmail.com>
2021-08-02 21:02:13 -0300

    refactor: use lombok annotations in project classes

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/exceptions/Violation.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/exceptions/Violation.java
@@ -1,27 +1,9 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.exceptions;
 
+import lombok.Value;
+
+@Value
 public class Violation {
-    private String name;
-    private String reason;
-
-    public Violation(String name, String reason) {
-        this.name = name;
-        this.reason = reason;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
+    String name;
+    String reason;
 }
--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/UserCreateDto.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/UserCreateDto.java
@@ -1,37 +1,21 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.user;
 
+import lombok.Value;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Value
 public class UserCreateDto {
     @NotNull
     @NotBlank
     @Email
-    private String email;
+    String email;
 
     @NotNull
     @NotBlank
     @Size(min = 6, max = 22)
-    private String password;
-
-    public UserCreateDto() {
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    String password;
 }
