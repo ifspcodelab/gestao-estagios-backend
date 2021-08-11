75e2dfb6f4c461d0b90ab2ea7cd5af50a0440d2c
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-08-11 18:09:16 -0300

    test: use factory in campus class unit test

--- src/test/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/campus/CampusTest.java
+++ src/test/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/campus/CampusTest.java
@@ -8,21 +8,7 @@ public class CampusTest {
 
     @Test
     public void shouldGenerateIdWhenParameterizedConstructorIsCalled() {
-        String name = "Test Campus";
-        String abbreviation = "TCS";
-        String postalCode = "123456";
-        String street = "Test Street";
-        String neighborhood = "Test Neighborhood";
-        String city = "Test City";
-        String state = "Test State";
-        String number = "123456";
-        String complement = "Test Complement";
-        Address address = new Address(postalCode, street, neighborhood, city, state, number, complement);
-        String telephone = "1234-5678";
-        String email = "testcampus@email.com";
-        String website = "https://testcampus.com";
-
-        Campus campus = new Campus(name, abbreviation, address, telephone, email, website);
+        Campus campus = CampusFactory.sampleCampus();
 
         assertThat(campus.getId()).isNotNull();
     }
