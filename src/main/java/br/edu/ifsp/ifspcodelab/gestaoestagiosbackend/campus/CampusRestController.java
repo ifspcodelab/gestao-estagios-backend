71f662140f9e0477a9aab9770e66aa54eb1be47d
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-08-14 13:40:53 -0300

    refactor: enable cors in campus controller

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/campus/CampusRestController.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/campus/CampusRestController.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/campuses")
+@CrossOrigin
 @AllArgsConstructor
 public class CampusRestController {
     private final CampusService campusService;
