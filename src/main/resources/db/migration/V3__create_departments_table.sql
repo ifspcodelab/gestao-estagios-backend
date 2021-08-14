8ca3c7df2df505d1a3e6ca70010e37bc8d6f5912
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-08-14 13:42:35 -0300

    feat: create departments table migration

---
+++ src/main/resources/db/migration/V3__create_departments_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE departments(
+     id UUID NOT NULL,
+     name VARCHAR NOT NULL,
+     abbreviation VARCHAR NOT NULL,
+     campus_id UUID NOT NULL,
+     CONSTRAINT departments_pk PRIMARY KEY (id),
+     CONSTRAINT campuses_fk FOREIGN KEY (campus_id) REFERENCES campuses(id)
+);
