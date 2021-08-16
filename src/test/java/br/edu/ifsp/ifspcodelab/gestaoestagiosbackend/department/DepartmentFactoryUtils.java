809a186eb8647910efd5b53e7a4bcfa9adff2ba2
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-08-16 19:12:33 -0300

    test: add unit test for department class

---
+++ src/test/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/department/DepartmentFactoryUtils.java
@@ -0,0 +1,12 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.department;
+
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.campus.Campus;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.campus.CampusFactoryUtils;
+
+public class DepartmentFactoryUtils {
+    public static Department sampleDepartment() {
+        Campus campus = CampusFactoryUtils.sampleCampus();
+        return new Department("Test Department", "TDP", campus);
+    }
+
+}
