4eab489ce921b23210638e3eb6a9e5f6dcbae95e
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-08-23 18:47:12 -0300

    test: add unit test for course class

---
+++ src/test/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/course/CourseTest.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.course;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CourseTest {
+
+    @Test
+    public void shouldGenerateIdWhenParameterizedConstructorIsCalled() {
+        Course course = CourseFactoryUtils.sampleCourse();
+
+        assertThat(course.getId()).isNotNull();
+    }
+}
