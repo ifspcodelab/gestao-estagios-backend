ea74d17a112a7f4cdb14dec505be04f7d082382a
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-08-25 18:23:38 -0300

    refactor: improve course update status dto and service

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/course/CourseUpdateStatusDto.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/course/CourseUpdateStatusDto.java
@@ -1,15 +1,12 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.course;
 
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.enums.EntityStatus;
-import lombok.Value;
+import lombok.Data;
 
 import javax.validation.constraints.NotNull;
-import java.util.UUID;
 
-@Value
+@Data
 public class CourseUpdateStatusDto {
     @NotNull
     EntityStatus status;
-    @NotNull
-    UUID departmentId;
 }
