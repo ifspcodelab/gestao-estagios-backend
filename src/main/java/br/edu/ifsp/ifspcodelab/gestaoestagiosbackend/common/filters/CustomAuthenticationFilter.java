026f1cf1c07afef836a104277823639343c8a0a8
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-09-23 23:17:51 -0300

    feat: handle unsuccessful auth and get credentials from json

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/filters/CustomAuthenticationFilter.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/filters/CustomAuthenticationFilter.java
@@ -5,18 +5,23 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,8 +36,16 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        String registration = request.getParameter("registration");
-        String password = request.getParameter("password");
+        String registration, password;
+        try {
+            Map<String, String> requestMap = new ObjectMapper().readValue(request.getInputStream(), Map.class);
+            registration = requestMap.get("registration");
+            password = requestMap.get("password");
+        } catch (IOException e) {
+            throw new AuthenticationServiceException(e.getMessage());
+        }
+//        String registration = request.getParameter("registration");
+//        String password = request.getParameter("password");
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(registration, password);
         return authenticationManager.authenticate(authenticationToken);
     }
@@ -59,5 +72,23 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         tokens.put("refresh_token", refresh_token);
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", failed.getMessage());
+        body.put("error", "Username or password is incorrect");
+        body.put("timeStamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s")));
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+
+        PrintWriter out = response.getWriter();
+        out.print(new ObjectMapper().writeValueAsString(body));
+        out.flush();
     }
 }
