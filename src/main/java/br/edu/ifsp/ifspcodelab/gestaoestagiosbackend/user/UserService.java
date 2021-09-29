cca02e60ab4eb19f5a170f14b7576010a04e687f
Fabio Mendes <fabiomendes53@gmail.com>
2021-09-29 10:53:37 -0300

    feat: create student rest controller

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/UserService.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/UserService.java
@@ -2,10 +2,12 @@ package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.user;
 
 
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.advisor.Advisor;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.student.Student;
 
 import java.util.List;
 
 public interface UserService {
     Advisor createAdvisor(UserAdvisorCreateDto userAdvisorCreateDto);
     List<User> findAll();
+    Student createStudent(UserStudentCreateDto userStudentCreateDto);
 }
