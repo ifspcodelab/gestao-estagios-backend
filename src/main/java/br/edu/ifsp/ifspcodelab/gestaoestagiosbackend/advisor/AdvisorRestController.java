ca78b75d6335958e20ec30c462e75aa878848d74
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-01 13:48:11 -0300

    feat: implements student update endpoint

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/advisor/AdvisorRestController.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/advisor/AdvisorRestController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/advisors")
+@CrossOrigin
 @AllArgsConstructor
 public class AdvisorRestController {
     private final UserService userService;
--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/student/StudentService.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/student/StudentService.java
@@ -1,6 +1,8 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.student;
 
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.curriculum.Curriculum;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.user.UserDto;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.user.UserStudentCreateDto;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +11,6 @@ public interface StudentService {
     Student create(Student student);
     List<Student> findAll();
     Curriculum getCurriculum(UUID curriculumId, UUID courseId);
+    Student findById(UUID id);
+    UserDto update(UserDto userDto);
 }
