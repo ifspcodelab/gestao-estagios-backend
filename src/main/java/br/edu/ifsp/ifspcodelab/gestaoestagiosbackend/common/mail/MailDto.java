07519d680e8529b36efe0127984e5a07aa0a1be1
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-06 16:25:09 -0300

    feat: create mail dto class

---
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/mail/MailDto.java
@@ -0,0 +1,40 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail;
+
+import lombok.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Builder
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class MailDto {
+
+    private String msgHTML;
+    private String title;
+    private List<String> recipientsTO;
+    private List<String> recipientsCC;
+    private List<String> recipientsBCC;
+    private Date dateSent;
+    private List<File> files;
+
+    public MailDto setRecipientTo(String... destinary) {
+
+        recipientsTO = new ArrayList<>();
+
+        for(String s : destinary) {
+            recipientsTO.add(s);
+        }
+
+        return this;
+    }
+
+    public boolean isValid() {
+        return (!msgHTML.isBlank() && Objects.nonNull(msgHTML) && !title.isBlank() && Objects.nonNull(title) && Objects.nonNull(this.recipientsTO) && !this.recipientsTO.isEmpty());
+    }
+}
