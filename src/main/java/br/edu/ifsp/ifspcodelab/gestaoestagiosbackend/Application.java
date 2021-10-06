69017a88f0f282f1037fff4ae170be4dc4096eac
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-06 16:54:28 -0300

    feat: adjust mail configurations

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
@@ -1,8 +1,12 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend;
 
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.SmtpAuthenticator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.objenesis.instantiator.sun.SunReflectionFactorySerializationInstantiator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,5 +20,11 @@ public class Application {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Qualifier("mailer")
+	SmtpAuthenticator mailerAccount(@Value("${application.mail.username}") String username, @Value("${application.mail.password}") String password) {
+		return new SmtpAuthenticator(username, password);
 	}
 }
