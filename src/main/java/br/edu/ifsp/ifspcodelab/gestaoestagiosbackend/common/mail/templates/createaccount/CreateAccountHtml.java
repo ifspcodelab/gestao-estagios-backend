a2f7f6f3b9e79a00aa5d07147c57ce513731327e
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-07 14:13:24 -0300

    feat: add link to active account email

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/mail/templates/createaccount/CreateAccountHtml.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/mail/templates/createaccount/CreateAccountHtml.java
@@ -11,12 +11,21 @@ public abstract class CreateAccountHtml {
                 "    <meta charset=\"UTF-8\">\r\n" +
                 "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n" +
                 "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" +
+                "    <!-- CSS only -->\r\n" +
+                "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF\" crossorigin=\"anonymous\">\r\n" +
                 "</head>\r\n" +
                 "<body>\r\n" +
-                "    <p>Seja bem vindo $USERNAME,</p>\r\n" +
-                "    <a href=\"#\">\r\n" +
-                "        <p>Clique aqui para ativar sua conta.</p>\r\n" +
-                "    </a>\r\n" +
+                "    <div class=\"container\">\r\n" +
+                "        <div class=\"jumbotron\">\r\n" +
+                "            <h1 class=\"display-6\"><strong>Seja bem vindo(a), $USERNAME!</strong></h1>\r\n" +
+                "            <p class=\"lead\">Sistema de Apoio à Orientação de Estágios do Instituto Federal de São Paulo.</p>\r\n" +
+                "            <hr class=\"my-4\">\r\n" +
+                "            <p>Clique no botão abaixo para ativar sua conta e ter acesso ao sistema.</p>\r\n" +
+                "            <p class=\"lead\">\r\n" +
+                "              <a class=\"btn btn-primary btn-lg\" href=\"$BASEURL?token=$TOKEN\" role=\"button\">Ativar conta</a>\r\n" +
+                "            </p>\r\n" +
+                "        </div>\r\n" +
+                "    </div>\r\n" +
                 "</body>\r\n" +
                 "</html>";
     }
