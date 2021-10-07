cc3790d118f8872f4b56bfedcbd5078208222605
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-07 15:57:53 -0300

    feat: send email to register student

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/mail/config/CreatorParametersMail.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/mail/config/CreatorParametersMail.java
@@ -2,17 +2,18 @@ package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public abstract class CreatorParametersMail {
 
     private CreatorParametersMail() {}
 
-    public static Map<String, String> activateAccount(String username, String baseUrl, String token) {
+    public static Map<String, String> activateAccount(String username, String baseUrl, UUID userId) {
         Map<String, String> map = new HashMap<>();
 
         map.put("$USERNAME", username);
-        map.put("$BASE_URL", baseUrl);
-        map.put("$TOKEN", token);
+        map.put("$BASEURL", baseUrl);
+        map.put("$IDUSER", userId.toString());
 
         return map;
     }
