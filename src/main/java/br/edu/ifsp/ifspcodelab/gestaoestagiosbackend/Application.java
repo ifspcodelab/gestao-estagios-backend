78f6adb7810f290fc0d29c53c171efdc9b53e150
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-10-08 19:52:33 -0300

    refactor: improve sendermail and user methods

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.objenesis.instantiator.sun.SunReflectionFactorySerializationInstantiator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
