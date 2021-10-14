d545516452b0f3b5ab732e916e87620103075339
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-10-14 17:20:03 -0300

    feat: create user redefine password methods

---
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/user/UserForgetPasswordDto.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.user;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class UserForgetPasswordDto {
+    @NotNull
+    @NotBlank
+    String registration;
+}
