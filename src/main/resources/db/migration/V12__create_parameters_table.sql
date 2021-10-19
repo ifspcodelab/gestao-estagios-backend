5ee21d0b824c68847265f9c1918e148c8daaeb2e
GEovanna-Lima <geovannadasilvalima@gmail.com>
2021-10-19 14:40:54 -0300

    fix: bug sql codacy

--- src/main/resources/db/migration/V12__create_parameters_table.sql
+++ src/main/resources/db/migration/V12__create_parameters_table.sql
@@ -4,14 +4,13 @@ CREATE TABLE parameters(
      projectEquivalence VARCHAR NOT NULL,
      professionalEnjoyment VARCHAR NOT NULL,
      advisorRequestDeadline INT NOT NULL,
-)
+);
 INSERT INTO parameters (
      requiredOrNot,
      projectEquivalence,
      professionalEnjoyment,
      advisorRequestDeadline
 )
-
 VALUES (
     'mensagem para estágio obrigatório ou não obrigatório',
     'mensagem para equiparação de projeto institucional',
