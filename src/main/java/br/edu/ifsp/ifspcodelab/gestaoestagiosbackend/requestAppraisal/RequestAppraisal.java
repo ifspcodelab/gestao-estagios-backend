fbcb84a2cb5fde1e26c4eb2df3faefb4445ffc5d
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-19 15:12:00 -0300

    feat: create advisor request appraisal entity

---
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/requestAppraisal/RequestAppraisal.java
@@ -0,0 +1,46 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.requestAppraisal;
+
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.request.AdvisorRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "advisor_request_appraisals")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RequestAppraisal {
+
+    @Id
+    private UUID id;
+
+    private Instant createdAt;
+
+    private String details;
+
+    private Boolean idDeferred;
+
+    private Instant meetingDate;
+
+    @OneToOne
+    private AdvisorRequest advisorRequest;
+
+    public RequestAppraisal(String details, Boolean idDeferred, Instant meetingDate, AdvisorRequest advisorRequest) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.details = details;
+        this.idDeferred = idDeferred;
+        this.meetingDate = meetingDate;
+        this.advisorRequest = advisorRequest;
+    }
+}
