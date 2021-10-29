82bc4a6f42068549fbce6e0bc28cb18968d3ae17
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-29 17:58:33 -0300

    feat: add annotation for schedule initialization

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
@@ -1,15 +1,20 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend;
 
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.SmtpAuthenticator;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.request.RequestExpiresVerification;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableScheduling
+@Import(RequestExpiresVerification.class)
 public class Application {
 
 	public static void main(String[] args) {
