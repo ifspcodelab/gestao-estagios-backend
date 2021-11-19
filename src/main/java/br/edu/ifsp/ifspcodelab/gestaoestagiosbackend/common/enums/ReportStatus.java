d54af52e323c8babc12ba16858ed8bd2c1b2d372
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-11-18 21:01:53 -0300

    feat: create monthly report class and migration

---
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/enums/ReportStatus.java
@@ -0,0 +1,8 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.enums;
+
+public enum ReportStatus {
+    DRAFT_PENDING,
+    DRAFT_ACCEPTED,
+    FINAL_PENDING,
+    FINAL_ACCEPTED
+}
---
+++ src/main/resources/db/migration/V18__create_monthly_reports_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE monthly_reports(
+    id UUID NOT NULL,
+    month DATE NOT NULL,
+    acceptation_date DATE,
+    start_date DATE,
+    end_date DATE,
+    attachment_url VARCHAR,
+    status VARCHAR NOT NULL,
+    activity_plan_id UUID NOT NULL,
+    CONSTRAINT monthly_reports_pk PRIMARY KEY (id),
+    CONSTRAINT activity_plan_fk FOREIGN key (activity_plan_id) REFERENCES activity_plans(id)
+);
