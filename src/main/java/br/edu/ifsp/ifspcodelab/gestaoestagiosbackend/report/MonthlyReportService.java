526448b6047116b04d9522ebff2a5de081f81de5
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-11-19 01:39:44 -0300

    feat: create monthly report find by id method

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/report/MonthlyReportService.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/report/MonthlyReportService.java
@@ -3,7 +3,9 @@ package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.report;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.plan.ActivityPlan;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public interface MonthlyReportService {
     MonthlyReport create(LocalDate month, ActivityPlan activityPlan);
+    MonthlyReport findById(UUID id);
 }
--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/report/MonthlyReportServiceImpl.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/report/MonthlyReportServiceImpl.java
@@ -1,10 +1,13 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.report;
 
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.exceptions.ResourceName;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.exceptions.ResourceNotFoundException;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.plan.ActivityPlan;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -15,5 +18,11 @@ public class MonthlyReportServiceImpl implements MonthlyReportService {
     @Override
     public MonthlyReport create(LocalDate month, ActivityPlan activityPlan) {
         return monthlyReportRepository.save(new MonthlyReport(month, activityPlan));
+    }
+
+    @Override
+    public MonthlyReport findById(UUID id) {
+        return monthlyReportRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException(ResourceName.ACTIVITY_PLAN, id));
     }
 }
