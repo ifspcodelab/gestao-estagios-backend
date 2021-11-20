925c481f7b4889643ac675daf56ead1f30a051b0
Fabio Mendes <fabiomendes53@gmail.com>
2021-11-20 14:25:55 -0300

    feat: send notification mail for realization term submission

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/realizationterm/RealizationTermServiceImpl.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/realizationterm/RealizationTermServiceImpl.java
@@ -2,14 +2,21 @@ package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.realizationterm;
 
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.exceptions.ResourceName;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.exceptions.ResourceNotFoundException;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.MailDto;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.SenderMail;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.config.CreatorParametersMail;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.config.FormatterMail;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.mail.templates.createaccount.TemplatesHtml;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.upload.UploadService;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.internship.Internship;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.internship.InternshipService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -18,9 +25,14 @@ public class RealizationTermServiceImpl implements RealizationTermService {
     private final RealizationTermRepository realizationTermRepository;
     private InternshipService internshipService;
     private UploadService uploadService;
+    private final SenderMail senderMail;
 
-    public RealizationTermServiceImpl(RealizationTermRepository realizationTermRepository) {
+    @Value("${application.frontend.url}")
+    private String frontendUrl;
+
+    public RealizationTermServiceImpl(RealizationTermRepository realizationTermRepository, SenderMail senderMail) {
         this.realizationTermRepository = realizationTermRepository;
+        this.senderMail = senderMail;
     }
 
     @Autowired
@@ -53,10 +65,11 @@ public class RealizationTermServiceImpl implements RealizationTermService {
     }
 
     @Override
+    @Transactional
     public RealizationTerm update(UUID internshipId,
                                   UUID realizationTermId,
                                   RealizationTermUpdateDto realizationTermUpdateDto) {
-        internshipService.findById(internshipId);
+        Internship internship = internshipService.findById(internshipId);
 
         RealizationTerm realizationTerm = realizationTermRepository.findById(realizationTermId)
                 .orElseThrow(() -> new ResourceNotFoundException(ResourceName.REALIZATION_TERM, realizationTermId));
@@ -64,7 +77,38 @@ public class RealizationTermServiceImpl implements RealizationTermService {
         realizationTerm.setTermStartDate(realizationTermUpdateDto.getTermStartDate());
         realizationTerm.setTermEndDate(realizationTermUpdateDto.getTermEndDate());
 
-        return realizationTermRepository.save(realizationTerm);
+        RealizationTerm realizationTermSaved = realizationTermRepository.save(realizationTerm);
+
+        MailDto studentEmail = MailDto.builder()
+                .title("Termo de Realização de Estágio enviado")
+                .msgHTML(TemplatesHtml.getRealizationTermSubmissionStudent())
+                .build();
+
+        Map<String, String> paramsStudentEmail = CreatorParametersMail.setRealizationTermSubmissionStudent(
+                internship.getAdvisorRequest().getStudent().getUser().getName()
+        );
+        studentEmail = FormatterMail.build(studentEmail, paramsStudentEmail);
+        studentEmail.setRecipientTo(internship.getAdvisorRequest().getStudent().getUser().getEmail());
+
+        MailDto advisorEmail = MailDto.builder()
+                .title(
+                        "Novo Termo de Realização de Estágio recebido [" +
+                                internship.getAdvisorRequest().getStudent().getUser().getName() + "]")
+                .msgHTML(TemplatesHtml.getRealizationTermSubmissionAdvisor())
+                .build();
+
+        Map<String, String> paramsAdvisorEmail = CreatorParametersMail.setRealizationTermSubmissionAdvisor(
+                internship.getAdvisorRequest().getAdvisor().getUser().getName(),
+                internship.getAdvisorRequest().getStudent().getUser().getName(),
+                frontendUrl
+        );
+        advisorEmail = FormatterMail.build(advisorEmail, paramsAdvisorEmail);
+        advisorEmail.setRecipientTo(internship.getAdvisorRequest().getAdvisor().getUser().getEmail());
+
+        senderMail.sendEmail(studentEmail);
+        senderMail.sendEmail(advisorEmail);
+
+        return realizationTermSaved;
     }
 
     private String getRealizationTermFileName(Internship internship) {
