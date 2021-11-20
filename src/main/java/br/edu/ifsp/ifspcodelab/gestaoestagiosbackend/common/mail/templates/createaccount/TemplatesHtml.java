cb0915bfd66fa6dd93b5feeebf681ee2dbd7a970
Fabio Mendes <fabiomendes53@gmail.com>
2021-11-20 15:22:57 -0300

    feat: create email templates for realization term appraisal

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/mail/templates/createaccount/TemplatesHtml.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/common/mail/templates/createaccount/TemplatesHtml.java
@@ -924,4 +924,150 @@ public abstract class TemplatesHtml {
                 "\n" +
                 "</html>";
     }
+
+    public static String getRealizationTermApproved() {
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"pt-br\">\n" +
+                "\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n" +
+                "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n" +
+                "    <link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap\" rel=\"stylesheet\">\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Roboto, \"Helvetica Neue\", sans-serif;\n" +
+                "            color: #494949;\n" +
+                "        }\n" +
+                "\n" +
+                "        .button-activate {\n" +
+                "            padding-left: 2.5rem;\n" +
+                "            padding-right: 2.5rem;\n" +
+                "            background-color: #673ab7;\n" +
+                "        }\n" +
+                "\n" +
+                "        .button-row a {\n" +
+                "            text-align: center;\n" +
+                "            margin-left: auto;\n" +
+                "            margin-right: auto;\n" +
+                "            padding-left: 1.5rem;\n" +
+                "            padding-right: 1.5rem;\n" +
+                "            padding-top: 10px;\n" +
+                "            padding-bottom: 10px;\n" +
+                "            color: #fff;\n" +
+                "            font-size: 14px;\n" +
+                "            font-weight: 500;\n" +
+                "            text-decoration: none;\n" +
+                "            border-radius: 4px;\n" +
+                "        }\n" +
+                "\n" +
+                "        .lead {\n" +
+                "            font-weight: 400;\n" +
+                "        }\n" +
+                "\n" +
+                "        span {\n" +
+                "            font-weight: 700;\n" +
+                "        }\n" +
+                "\n" +
+                "        .activate {\n" +
+                "            margin-bottom: 30px;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>\n" +
+                "    <div class=\"container\">\n" +
+                "        <div class=\"jumbotron\">\n" +
+                "            <h1 class=\"display-6\"><span>Termo de Realização de Estágio deferido!</span></h1>\n" +
+                "            <p class=\"lead\">Sistema de Apoio à Orientação de Estágios do Instituto Federal de São Paulo.</p>\n" +
+                "            <hr class=\"my-4\">\n" +
+                "            <p>Prezado(a) $STUDENTNAME, </p>" +
+                "            <p>Informamos que o orientador $ADVISORNAME avaliou seu <b>Termo de Realização de Estágio</b> e marcou como deferido.</p>" +
+                "            <br/>" +
+                "            <p class=\"activate\">Avaliação: </p>" +
+                "            <span style=\"white-space: pre-line\">$DETAILS</span>\n" +
+                "        </div>\n" +
+                "    </div>\n" +
+                "</body>\n" +
+                "\n" +
+                "</html>";
+    }
+
+    public static String getRealizationTermIndeferred() {
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"pt-br\">\n" +
+                "\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n" +
+                "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n" +
+                "    <link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap\" rel=\"stylesheet\">\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-family: Roboto, \"Helvetica Neue\", sans-serif;\n" +
+                "            color: #494949;\n" +
+                "        }\n" +
+                "\n" +
+                "        .button-activate {\n" +
+                "            padding-left: 2.5rem;\n" +
+                "            padding-right: 2.5rem;\n" +
+                "            background-color: #673ab7;\n" +
+                "        }\n" +
+                "\n" +
+                "        .button-row a {\n" +
+                "            text-align: center;\n" +
+                "            margin-left: auto;\n" +
+                "            margin-right: auto;\n" +
+                "            padding-left: 1.5rem;\n" +
+                "            padding-right: 1.5rem;\n" +
+                "            padding-top: 10px;\n" +
+                "            padding-bottom: 10px;\n" +
+                "            color: #fff;\n" +
+                "            font-size: 14px;\n" +
+                "            font-weight: 500;\n" +
+                "            text-decoration: none;\n" +
+                "            border-radius: 4px;\n" +
+                "        }\n" +
+                "\n" +
+                "        .lead {\n" +
+                "            font-weight: 400;\n" +
+                "        }\n" +
+                "\n" +
+                "        span {\n" +
+                "            font-weight: 700;\n" +
+                "        }\n" +
+                "\n" +
+                "        .activate {\n" +
+                "            margin-bottom: 30px;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>\n" +
+                "    <div class=\"container\">\n" +
+                "        <div class=\"jumbotron\">\n" +
+                "            <h1 class=\"display-6\"><span>Termo de Realização de Estágio indeferido.</span></h1>\n" +
+                "            <p class=\"lead\">Sistema de Apoio à Orientação de Estágios do Instituto Federal de São Paulo.</p>\n" +
+                "            <hr class=\"my-4\">\n" +
+                "            <p>Prezado(a) $STUDENTNAME, </p>" +
+                "            <p>Informamos que o orientador $ADVISORNAME avaliou e indeferiu seu <b>Termo de Realização de Estágio</b>.</p>" +
+                "            <p class=\"activate\">Detalhes da avaliação: </p>" +
+                "            <span style=\"white-space: pre-line\">$DETAILS</span>\n" +
+                "            <br/> \n" +
+                "            <p>É necessário reenviar o Termo com as correções solicitadas pelo orientador.</p>" +
+                "            <br/><br/><br/>" +
+                "                <div class=\"button-row\">\n" +
+                "                    <a class=\"button-activate\" href=\"$SYSTEMLINK\" role=\"button\">Acessar o sistema</a>\n" +
+                "                </div>\n" +
+                "            <br/>" +
+                "        </div>\n" +
+                "    </div>\n" +
+                "</body>\n" +
+                "\n" +
+                "</html>";
+    }
 }
