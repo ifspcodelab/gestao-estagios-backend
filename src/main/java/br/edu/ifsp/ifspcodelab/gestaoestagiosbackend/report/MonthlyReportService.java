dc7d949ca7bc679477ba99d2f2f931e5c64be6f2
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-11-21 22:46:59 -0300

    feat: create send attachment method in monthly report

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/report/MonthlyReportService.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/report/MonthlyReportService.java
@@ -1,6 +1,7 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.report;
 
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.plan.ActivityPlan;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -8,5 +9,6 @@ import java.util.UUID;
 public interface MonthlyReportService {
     MonthlyReport create(LocalDate month, ActivityPlan activityPlan);
     MonthlyReport update(MonthlyReport monthlyReport);
+    MonthlyReport sendAttachment(UUID internshipId, UUID monthlyReportId, MultipartFile file);
     MonthlyReport findById(UUID id);
 }
