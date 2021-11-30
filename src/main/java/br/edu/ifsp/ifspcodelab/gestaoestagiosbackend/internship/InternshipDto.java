3f8cce62948974eb38e3fc2a39c1ff7200202a8e
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-11-30 14:10:43 -0300

    feat: put realization term in internship dto

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/internship/InternshipDto.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/internship/InternshipDto.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.internship;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.enums.InternshipStatus;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.enums.InternshipType;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.plan.ActivityPlan;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.realizationterm.RealizationTerm;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.report.MonthlyReport;
 import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.request.AdvisorRequestDto;
 import lombok.Value;
@@ -18,4 +19,5 @@ public class InternshipDto {
     AdvisorRequestDto advisorRequest;
     Set<ActivityPlan> activityPlans;
     Set<MonthlyReport> monthlyReports;
+    Set<RealizationTerm> realizationTerms;
 }
