4727873a319c49c898a442cae2100663b57fda20
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-12-03 17:55:44 -0300

    fix: remove non-nullable annotati from update dto field

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/plan/ActivityPlanUpdateDto.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/plan/ActivityPlanUpdateDto.java
@@ -2,14 +2,11 @@ package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.plan;
 
 import lombok.Value;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Value
 public class ActivityPlanUpdateDto {
-    @NotNull
-    @NotBlank
     String companyName;
     @NotNull
     LocalDate internshipStartDate;
