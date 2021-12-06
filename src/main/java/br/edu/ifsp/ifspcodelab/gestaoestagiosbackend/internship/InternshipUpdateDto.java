783e03fca776b73d907185294050e9e69e2a1fdd
Fabio Mendes <fabiomendes53@gmail.com>
2021-12-06 14:43:02 -0300

    refactor: remove internship update dto

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/internship/InternshipUpdateDto.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.internship;
-
-import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.enums.InternshipStatus;
-import lombok.Value;
-
-@Value
-public class InternshipUpdateDto {
-    InternshipStatus status;
-}
