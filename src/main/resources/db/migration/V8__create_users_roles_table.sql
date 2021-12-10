eba6f4711f09397d686c132940f29a1209458bd8
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-12-10 14:01:04 -0300

    feat: add user role insert in migration

--- src/main/resources/db/migration/V8__create_users_roles_table.sql
+++ src/main/resources/db/migration/V8__create_users_roles_table.sql
@@ -3,3 +3,5 @@ CREATE TABLE users_roles(
     role VARCHAR NOT NULL,
     CONSTRAINT users_fk FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+INSERT INTO users_roles VALUES ('9dfc3dbf-b2ca-4111-95f4-074d9cd847d1', 'ROLE_ADMIN');
