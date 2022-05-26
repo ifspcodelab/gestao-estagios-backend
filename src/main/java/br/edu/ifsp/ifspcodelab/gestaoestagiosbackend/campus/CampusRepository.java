b85b3e9b36753a2a4f659450b46fe4366d2dc0a4
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-05-26 16:39:09 -0300

    feat: add method to find all campuses by status

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/campus/CampusRepository.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/campus/CampusRepository.java
@@ -1,9 +1,12 @@
 package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.campus;
 
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.common.enums.EntityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -14,4 +17,7 @@ public interface CampusRepository extends JpaRepository<Campus, UUID> {
     boolean existsByEmailExcludedId(String email, UUID id);
     @Query("select count(c) > 0 from Campus c where c.abbreviation = ?1 and c.id <> ?2")
     boolean existsByAbbreviationExcludedId(String abbreviation, UUID id);
+    @Query("select u from Campus u where u.status = :status")
+    List<Campus> findAllByStatus (@Param("status") EntityStatus status);
+
 }
