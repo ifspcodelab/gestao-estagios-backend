75c94501dd58227e8fd474fb6a59bbaa89a040f5
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-06-12 08:57:08 -0300

    fix: error with RequestExpiresVerification bean
    The @DataJpaTeste annotation disables Spring's automatic configuration, as a result, it is unable to @Import the RequestExpiresVerification class in the tests, generating the error of not being able to create its bean. Since the @Import annotation is not needed, so the solution was to remove it.

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/Application.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableScheduling
-@Import(RequestExpiresVerification.class)
 public class Application {
 
 	public static void main(String[] args) {
