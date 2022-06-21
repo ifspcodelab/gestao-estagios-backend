d5220042a2ec3e8c37fa260841aee0701f33d2f6
isabella-66 <amasprog@gmail.com>
2022-06-21 18:42:17 -0300

    feat: add max validation to advisor request deadline

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/parameter/ParameterCreateDto.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/parameter/ParameterCreateDto.java
@@ -5,6 +5,7 @@ import lombok.Value;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constrains.Max;
 
 @Value
 public class ParameterCreateDto {
@@ -19,6 +20,7 @@ public class ParameterCreateDto {
     String professionalValidationMessage;
     @NotNull
     @Positive
+    @Max(value = 12)
     Integer advisorRequestDeadline;
     @NotNull
     @Positive
