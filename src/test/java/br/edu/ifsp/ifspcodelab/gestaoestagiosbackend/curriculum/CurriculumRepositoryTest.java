4d676da0ac994b17f844ebe6ebde253c09a86b3a
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-06-29 21:51:29 -0300

    test: add curriculumrepositorytest class

---
+++ src/test/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/curriculum/CurriculumRepositoryTest.java
@@ -0,0 +1,38 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.curriculum;
+
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.campus.Campus;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.campus.CampusFactoryUtils;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.city.City;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.city.CityFactoryUtils;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.course.Course;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.course.CourseFactoryUtils;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.department.Department;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.department.DepartmentFactoryUtils;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.state.State;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.state.StateFactoryUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class CurriculumRepositoryTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CurriculumRepository curriculumRepository;
+
+    private Curriculum curriculum;
+
+    @BeforeEach
+    public void setUp() {
+        State state = entityManager.persistAndFlush(StateFactoryUtils.sampleState());
+        City city = entityManager.persistAndFlush(CityFactoryUtils.sampleCity(state));
+        Campus campus = entityManager.persistAndFlush(CampusFactoryUtils.sampleCampus(city));
+        Department department = entityManager.persistAndFlush(DepartmentFactoryUtils.sampleDepartment(campus));
+        Course course = entityManager.persistAndFlush(CourseFactoryUtils.sampleCourse(department));
+        curriculum = CurriculumFactoryUtils.sampleCurriculum(course);
+    }
+
+}
