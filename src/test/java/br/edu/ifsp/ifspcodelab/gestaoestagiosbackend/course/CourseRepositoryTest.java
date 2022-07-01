555ffab67091a3a11cc4b1181dcd39611ef70af0
Lucas Gabriel <lucasogjc@gmail.com>
2022-07-01 18:48:24 -0300

    test: adds courserepositorytest class with its setup

---
+++ src/test/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/course/CourseRepositoryTest.java
@@ -0,0 +1,35 @@
+package br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.course;
+
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.campus.Campus;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.campus.CampusFactoryUtils;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.city.City;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.city.CityFactoryUtils;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.department.Department;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.department.DepartmentFactoryUtils;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.state.State;
+import br.edu.ifsp.ifspcodelab.gestaoestagiosbackend.state.StateFactoryUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class CourseRepositoryTest {
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private CourseRepository courseRepository;
+
+    private Course course;
+
+    @BeforeEach
+    public void setUp() {
+        State state = entityManager.persistAndFlush(StateFactoryUtils.sampleState());
+        City city = entityManager.persistAndFlush(CityFactoryUtils.sampleCity(state));
+        Campus campus = entityManager.persistAndFlush(CampusFactoryUtils.sampleCampus(city));
+        Department department = entityManager.persistAndFlush(DepartmentFactoryUtils.sampleDepartment(campus));
+        course = CourseFactoryUtils.sampleCourse(department);
+    }
+
+    //boolean existsByAbbreviationAndDepartmentIdExcludedId(String abbreviation, UUID departmentId, UUID courseId);
+}
