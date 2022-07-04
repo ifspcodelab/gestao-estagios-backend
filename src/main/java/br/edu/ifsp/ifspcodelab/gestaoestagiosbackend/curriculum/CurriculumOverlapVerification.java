7a649e92b27c89e76c8412cb4898bbc9fd90128c
Bruna S. Pires <69404377+piresbruna@users.noreply.github.com>
2022-07-04 01:10:05 -0300

    refactor: improves logic operator

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/curriculum/CurriculumOverlapVerification.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/curriculum/CurriculumOverlapVerification.java
@@ -12,12 +12,12 @@ public class CurriculumOverlapVerification {
     public boolean checkingAddCurriculum(List<Curriculum> curriculums, Curriculum curriculum){
         var validityStartDate = curriculum.getValidityStartDate();
         var validityEndDate = curriculum.getValidityEndDate();
-        if(!(validityEndDate == null)) {
+        if(validityEndDate != null) {
             verifyDatesChronology(validityStartDate, validityEndDate);
             for(Curriculum c : curriculums){
                 var comparingStartDate = c.getValidityStartDate();
                 var comparingEndDate = c.getValidityEndDate();
-                if(!(comparingEndDate == null) && (
+                if(comparingEndDate != null && (
                         (validityStartDate.isAfter(comparingStartDate) && validityStartDate.isBefore(comparingEndDate)) || (validityEndDate.isAfter(comparingStartDate) && validityEndDate.isBefore(comparingEndDate)) || validityStartDate.isEqual(comparingStartDate) || validityStartDate.isEqual(comparingEndDate) || validityEndDate.isEqual(comparingStartDate) || validityEndDate.isEqual(comparingEndDate))
                 ){
                     throw new TimeOverlayException(validityStartDate,validityEndDate,comparingStartDate,comparingEndDate);
@@ -55,12 +55,12 @@ public class CurriculumOverlapVerification {
                 var comparingStartDate = c.getValidityStartDate();
                 var comparingEndDate = c.getValidityEndDate();
                 if(
-                        (!(comparingEndDate == null) && !(c.getId() == curriculumID)) && ((validityStartDate.isAfter(comparingStartDate) && validityStartDate.isBefore(comparingEndDate)) || (validityEndDate.isAfter(comparingStartDate) && validityEndDate.isBefore(comparingEndDate)) || validityStartDate.isEqual(comparingStartDate) || validityStartDate.isEqual(comparingEndDate) || validityEndDate.isEqual(comparingStartDate) || validityEndDate.isEqual(comparingEndDate))
+                        (comparingEndDate != null && c.getId() != curriculumID) && ((validityStartDate.isAfter(comparingStartDate) && validityStartDate.isBefore(comparingEndDate)) || (validityEndDate.isAfter(comparingStartDate) && validityEndDate.isBefore(comparingEndDate)) || validityStartDate.isEqual(comparingStartDate) || validityStartDate.isEqual(comparingEndDate) || validityEndDate.isEqual(comparingStartDate) || validityEndDate.isEqual(comparingEndDate))
                 ){
                     throw  new TimeOverlayException(validityStartDate, validityEndDate, comparingStartDate, comparingEndDate);
                 }
                 else if (
-                        (comparingEndDate == null && !(c.getId() == curriculumID)) && (validityStartDate.isAfter(comparingStartDate) || validityEndDate.isAfter(comparingStartDate) || validityStartDate.isEqual(comparingStartDate) || validityEndDate.isEqual(comparingStartDate))
+                        (comparingEndDate == null && c.getId() != curriculumID) && (validityStartDate.isAfter(comparingStartDate) || validityEndDate.isAfter(comparingStartDate) || validityStartDate.isEqual(comparingStartDate) || validityEndDate.isEqual(comparingStartDate))
                 ){
                     throw new TimeOverlayException(validityStartDate, validityEndDate,comparingStartDate,LocalDate.now());
                 }
@@ -71,7 +71,7 @@ public class CurriculumOverlapVerification {
                 var comparingStartDate = c.getValidityStartDate();
                 var comparingEndDate = c.getValidityEndDate();
                 if(
-                        !(c.getId() == curriculumID) && (validityStartDate.isBefore(comparingStartDate) || validityStartDate.isBefore(comparingEndDate) || validityStartDate.isEqual(comparingStartDate) || validityStartDate.isEqual(comparingEndDate))
+                        c.getId() != curriculumID && (validityStartDate.isBefore(comparingStartDate) || validityStartDate.isBefore(comparingEndDate) || validityStartDate.isEqual(comparingStartDate) || validityStartDate.isEqual(comparingEndDate))
                 ){
                     throw new TimeOverlayException(validityStartDate,LocalDate.now(),comparingStartDate,comparingEndDate);
                 }
