20f2e0e2f55251aedd9ee29a2544b34f3bc7561a
Irina Ferreira <irina.f@aluno.ifsp.edu.br>
2023-05-17 10:10:29 -0300

    fix: format code

--- src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/department/DepartmentRepository.java
+++ src/main/java/br/edu/ifsp/ifspcodelab/gestaoestagiosbackend/department/DepartmentRepository.java
@@ -15,9 +15,7 @@ public interface DepartmentRepository extends JpaRepository<Department, UUID> {
     boolean existsByAbbreviationAndCampusId(String abbreviation, UUID campusId);
     long countAllByCampusId(UUID campusId);
     List<Department> findAllByCampusId(UUID campusId);
-
     List<Department> findAllByCampusIdAndStatus(UUID campusId, EntityStatus status);
-
     Optional<Department> findByCampusIdAndId(UUID campusId, UUID departmentId);
     @Query("select count(d) > 0 from Department d join Campus as c on d.campus = c.id where d.abbreviation = ?1 and c.id = ?2 and d.id <> ?3")
     boolean existsByAbbreviationAndCampusIdExcludedId(String abbreviation, UUID campusId, UUID departmentId);
